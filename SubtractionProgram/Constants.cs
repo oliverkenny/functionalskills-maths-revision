6da32f4624d7167cab2ee2f73ff4c7ee346e9094
oliverkenny <oliver.j.kenny@gmail.com>
2019-09-18 22:06:14 +0100

    Fix #3 create constants for error messages

---
+++ SubtractionProgram/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace SubtractionProgram
+{
+	public static class Constants
+	{
+		public const string StrInvalidValue = "The value you entered was invalid. Please try again.";
+		public const string StrValueOutOfRange = "The value you entered was out of range. Please try again.";
+	}
+}
